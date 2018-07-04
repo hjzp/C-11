1c0f12c650cfe865d62750f1dd400c4151a02eab
hjzp <807961446@qq.com>
2018-07-04 19:56:41 +0800

    修改智能指针的实现

--- SmartPointer.hpp
+++ SmartPointer.hpp
@@ -8,26 +8,42 @@ public:
 	UniquePtr()
 	{
 		pointer = nullptr;
+		use_count = nullptr;
 	}
 	// 
 	UniquePtr(T* p)
 	{
 		pointer = p;
 		p = nullptr;
-		use_count = 1;
+		use_count = new int;
+		*use_count = 1;
 	}
-	// move constructor
-	UniquePtr(UniquePtr<T>&& that)
+	// copy constructor,release "other" object.
+	UniquePtr(UniquePtr<T>& other)
 	{
-		pointer = p;
-		p = nullptr;
-		++use_count;
+		pointer = other.pointer;
+		other.pointer = nullptr;
+		use_count = other.use_count;
+		other.use_count = nullptr;
+		//++(*use_count);  // unique pointer
+	}
+	// move constructor,release "other" object.
+	UniquePtr(UniquePtr<T>&& other)
+	{
+		pointer = other.pointer;
+		other.pointer = nullptr;
+		use_count = other.use_count;
+		other.use_count = nullptr;
+		//++(*use_count);  // unique pointer
 	}
 	//
-	UniquePtr<T>& operator=(const UniquePtr<T>& ptr)
+	UniquePtr<T>& operator=(const UniquePtr<T>& other)
 	{
-		pointer = ptr;
-		++use_count;
+		pointer = other.pointer;
+		other.pointer = nullptr;
+		use_count = other.use_count;
+		other.use_count = nullptr;
+		//++(*use_count);  // unique pointer
 	}
 	//
 	T& operator*()
@@ -42,14 +58,84 @@ public:
 	// 
 	~UniquePtr()
 	{
-		if (0 == --use_count)
+		if (use_count &&
+			(--(*use_count) == 0))
 		{
 			delete pointer;
+			pointer = nullptr;
+			delete use_count;
+			use_count = nullptr;
 		}
 	}
 
 private:
 	T* pointer;
-	int use_count;
+	int* use_count;
 };
 
+template <class T>
+class SharedPtr
+{
+public:
+	// 
+	SharedPtr()
+	{
+		pointer = nullptr;
+		use_count = nullptr;
+	}
+	// 
+	SharedPtr(T* p)
+	{
+		pointer = p;
+		p = nullptr;
+		use_count = new int;
+		*use_count = 1;
+	}
+	// copy constructor
+	SharedPtr(SharedPtr<T>& other)
+	{
+		pointer = other.pointer;
+		use_count = other.use_count;
+		++(*use_count);  // shared pointer
+	}
+	// move constructor
+	SharedPtr(SharedPtr<T>&& other)
+	{
+		pointer = other.pointer;
+		use_count = other.use_count;
+		++(*use_count);  // shared pointer
+	}
+	//
+	SharedPtr<T>& operator=(const SharedPtr<T>& other)
+	{
+		pointer = other.pointer;
+		use_count = other.use_count;
+		++(*use_count);  // shared pointer
+	}
+	//
+	T& operator*()
+	{
+		return (*pointer);
+	}
+	//
+	T* operator->()
+	{
+		return pointer;
+	}
+	// 
+	~SharedPtr()
+	{
+		if (use_count &&
+			(--(*use_count) == 0))
+		{
+			delete pointer;
+			pointer = nullptr;
+			delete use_count;
+			use_count = nullptr;
+		}
+	}
+
+private:
+	T* pointer;
+	int* use_count;
+};
