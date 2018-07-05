e6c95a9834a70e3df556bbfe69198a64c15554a9
hjzp <807961446@qq.com>
2018-07-05 17:25:27 +0800

    线程调用类方法

--- MultiThread.cpp
+++ MultiThread.cpp
@@ -5,6 +5,9 @@
 #include <iostream>
 #include <thread>
 #include <string>
+#include <iostream>
+
+using namespace std;
 
 // 线程传参问题
 
@@ -21,7 +24,7 @@ void run_thread_update_data()
 	std::cout << n << std::endl;
 }
 
-// 问题2：传入引用参数
+// 问题2：将局部变量传入线程
 void callable_with_arguments(double d, std::string const& s)
 {
 	std::cout << "d = " << d << ", s = " << s.c_str() << std::endl;
@@ -30,16 +33,18 @@ void run_thread_callable_with_arguments(float param)
 {
 	char buf[1024] = { 0 };
 	sprintf_s(buf, "%f", param);
-	std::thread t(callable_with_arguments, 3.14, std::string(buf));
-	t.detach();
+	std::thread t(callable_with_arguments, 3.14, std::string(buf)); // 最好不要直接传入局部变量buf，而是用string(buf)构造对象
+	t.detach();		// 不等待线程结束，局部变量被释放，如果直接将局部变量传入线程，可能会有问题。使用join()方法等待线程结束，肯定没问题
 }
 
+// 条件变量
 
-int main()
+
+class Class1
 {
-	run_thread_update_data();
-	run_thread_callable_with_arguments(1.2);
-
-    return 0;
-}
-
+public:
+	void task(int n)
+	{
+		cout << "MyThread1::task(), n = " << n << endl;
+	}
+};
